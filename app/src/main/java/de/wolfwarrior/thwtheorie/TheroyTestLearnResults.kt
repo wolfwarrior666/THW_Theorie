f2e60bb98d8a8ff0be184cccd17ab9fb78ed1097
Ian <81555659+wolfwarrior666@users.noreply.github.com>
2023-07-02 20:14:03 +0200

    - create activity

---
+++ app/src/main/java/de/wolfwarrior/thwtheorie/TheroyTestLearnResults.kt
@@ -0,0 +1,11 @@
+package de.wolfwarrior.thwtheorie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TheroyTestLearnResults : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theroy_test_learn_results)
+    }
+}
