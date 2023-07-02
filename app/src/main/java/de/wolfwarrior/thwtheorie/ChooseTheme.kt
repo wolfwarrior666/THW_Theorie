1dd9ce4838bacdad9c5372139383030f95c00977
Ian <81555659+wolfwarrior666@users.noreply.github.com>
2023-07-02 19:35:53 +0200

    - Add String for Lernbereichswahl - Give Buttons names and functions

--- app/src/main/java/de/wolfwarrior/thwtheorie/ChooseTheme.kt
+++ app/src/main/java/de/wolfwarrior/thwtheorie/ChooseTheme.kt
@@ -11,9 +11,39 @@ class ChooseTheme : AppCompatActivity() {
         setContentView(R.layout.activity_choose_theme)
     }
 
-    fun themeOn(view:View){
+    fun startActivity(num:Int){
         var intent = Intent(this,Theory::class.java)
-        intent.putExtra("Theme",1)
+        intent.putExtra("Theme",num)
         startActivity(intent)
+    }
+    fun themeOne(view:View){
+        startActivity(1)
+    }
+    fun themeTwo(view:View){
+        startActivity(2)
+    }
+    fun themeThree(view:View){
+        startActivity(3)
+    }
+    fun themeFour(view:View){
+        startActivity(4)
+    }
+    fun themeFive(view:View){
+        startActivity(5)
+    }
+    fun themeSix(view:View){
+        startActivity(6)
+    }
+    fun themeSeven(view:View){
+        startActivity(7)
+    }
+    fun themeEight(view:View){
+        startActivity(8)
+    }
+    fun themeNine(view:View){
+        startActivity(9)
+    }
+    fun themeTen(view:View){
+        startActivity(10)
     }
 }
