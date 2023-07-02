878e0cd2e42b1decb0a361f59d24a0ea4d837c59
Ian <81555659+wolfwarrior666@users.noreply.github.com>
2023-07-02 13:53:32 +0200

    Create new Activity for Theory

---
+++ app/src/main/java/de/wolfwarrior/thwtheorie/Theory.kt
@@ -0,0 +1,11 @@
+package de.wolfwarrior.thwtheorie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Theory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theory)
+    }
+}
