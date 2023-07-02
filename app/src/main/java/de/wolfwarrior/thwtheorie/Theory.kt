e6b70e33407e130350fe061bdb542bc4f2b51c23
Ian <81555659+wolfwarrior666@users.noreply.github.com>
2023-07-02 15:01:54 +0200

    - added untestet code

--- app/src/main/java/de/wolfwarrior/thwtheorie/Theory.kt
+++ app/src/main/java/de/wolfwarrior/thwtheorie/Theory.kt
@@ -7,5 +7,6 @@ class Theory : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_theory)
+
     }
 }
--- app/src/main/java/de/wolfwarrior/thwtheorie/logik/TheorieLogik.kt
+++ app/src/main/java/de/wolfwarrior/thwtheorie/logik/TheorieLogik.kt
@@ -1,4 +1,57 @@
 package de.wolfwarrior.thwtheorie.logik
 
-class TheorieLogik {
+import Question
+import android.util.Log
+import java.io.File
+
+private var currentLearnSet = mutableListOf<Question>()
+private var currentIndex = 0;
+private lateinit var currentQuestion:Question
+
+class TheorieLogik (val questions:List<Question>, val learnState:HashMap<String,Int>) {
+    fun loadDataFromOneChapter(chapterNumber: Int) {
+        currentLearnSet.clear()
+        currentIndex = 0;
+        for (q in questions) {
+            if (q.questionID.contains("$chapterNumber.")) {
+                currentLearnSet.add(q)
+            }
+        }
+    }
+
+    fun getLearnSetQuestion(): Question {
+        val currentQuestion = currentLearnSet[currentIndex];
+        currentIndex++
+        return currentQuestion;
+    }
+
+    fun checkAwnsers(answer: String):Boolean {
+       var correct = true;
+        if (answer.contains("A") == currentQuestion.answerA.rightOrWrong) {
+            Log.i("THWTheroy", "Answer A is correct")
+        }else{
+            correct = false
+        }
+
+        if (answer.contains("B") == currentQuestion.answerB.rightOrWrong) {
+            Log.i("THWTheroy", "Answer B is correct")
+        }else{
+            correct = false
+        }
+
+        if (answer.contains("C") == currentQuestion.answerC.rightOrWrong){
+            Log.i("THWTheroy", "Answer C is correct")
+        }else{
+            correct = false
+        }
+
+        if (correct){
+            if (learnState[currentQuestion.questionID] != null){
+                learnState[currentQuestion.questionID] = learnState[currentQuestion.questionID]!! + 1
+            }else{
+                learnState[currentQuestion.questionID] = 1
+            }
+        }
+        return correct
+    }
 }
