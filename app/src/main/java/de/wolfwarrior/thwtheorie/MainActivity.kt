40750e861e1aee8a799197c1c21a1f28a1222c22
Ian <81555659+wolfwarrior666@users.noreply.github.com>
2023-09-08 14:01:24 +0200

    Implementing Function from mainMenue (#9)
    * Adding Menu
    
    * Adjust Menue Opening Links
    
    * Adjust Menue Opening Links
    and Fix Warnings

--- app/src/main/java/de/wolfwarrior/thwtheorie/MainActivity.kt
+++ app/src/main/java/de/wolfwarrior/thwtheorie/MainActivity.kt
@@ -1,11 +1,14 @@
 package de.wolfwarrior.thwtheorie
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,22 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.showgithub -> {
+                showGitHub()
+                true
+            }
+            R.id.aboutthisapp -> {
+                aboutThisApp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
     @Suppress("UNUSED_PARAMETER")
     fun showList(view: View) {
         startActivity(Intent(this, ChooseTheme::class.java))
@@ -28,8 +47,23 @@ class MainActivity : AppCompatActivity() {
 
     @Suppress("UNUSED_PARAMETER")
     fun startExam(view: View){
-        var intent = Intent(this,Theory::class.java)
+        val intent = Intent(this,Theory::class.java)
         intent.putExtra("Theme",-2)
         startActivity(intent)
+    }
+
+
+    private fun aboutThisApp(){
+        val url = "https://github.com/wolfwarrior666/THW_Theorie#thw_theorie"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    private fun showGitHub(){
+        val url = "https://github.com/wolfwarrior666/THW_Theorie"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
