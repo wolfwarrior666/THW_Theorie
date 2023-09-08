3a6db4b630d07873ee6099e9ca7e5f6b698424c0
Ian <ian-wilken@mailbox.org>
2023-09-08 13:56:59 +0200

    Adding Menu

--- app/src/main/java/de/wolfwarrior/thwtheorie/MainActivity.kt
+++ app/src/main/java/de/wolfwarrior/thwtheorie/MainActivity.kt
@@ -1,11 +1,14 @@
 package de.wolfwarrior.thwtheorie
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,22 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.showgithub -> {
+                showGitHub()
+                true
+            }
+            R.id.aboutthisapp -> {
+                aboutThisApp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
     @Suppress("UNUSED_PARAMETER")
     fun showList(view: View) {
         startActivity(Intent(this, ChooseTheme::class.java))
@@ -31,5 +50,20 @@ class MainActivity : AppCompatActivity() {
         var intent = Intent(this,Theory::class.java)
         intent.putExtra("Theme",-2)
         startActivity(intent)
+    }
+
+
+    fun aboutThisApp(){
+        val url = "http://www.stackoverflow.com"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    fun showGitHub(){
+        val url = "http://www.stackoverflow.com"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
