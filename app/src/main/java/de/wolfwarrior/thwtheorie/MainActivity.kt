6b9dbcba447aca8d99085e3031d9f815a342211f
Ian <ian-wilken@mailbox.org>
2023-09-11 13:53:37 +0200

    Addign first Plot

--- app/src/main/java/de/wolfwarrior/thwtheorie/MainActivity.kt
+++ app/src/main/java/de/wolfwarrior/thwtheorie/MainActivity.kt
@@ -1,22 +1,63 @@
 package de.wolfwarrior.thwtheorie
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.WHITE
 import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.androidplot.xy.BarFormatter
+import com.androidplot.xy.SimpleXYSeries
+import com.androidplot.xy.XYPlot
+import com.androidplot.xy.XYSeries
 import com.google.android.material.appbar.MaterialToolbar
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var plot: XYPlot
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<MaterialToolbar>(R.id.main_menu_toolbar)
         setSupportActionBar(toolbar)
+
+
+        // initialize our XYPlot reference:
+        // initialize our XYPlot reference:
+        plot = findViewById<XYPlot>(R.id.myplot)
+
+        val wins: XYSeries = SimpleXYSeries(
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
+            "wins",
+            3,
+            4
+        )
+        val losses: XYSeries = SimpleXYSeries(
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
+            "losses",
+            0,
+            1
+        )
+
+        // draw wins bars with a green fill:
+        // draw wins bars with a green fill:
+        val winsFormatter = BarFormatter(Color.GREEN, Color.BLACK)
+        plot.addSeries(wins, winsFormatter)
+
+// draw losses bars with a red fill:
+
+// draw losses bars with a red fill:
+        val lossesFormatter = BarFormatter(Color.RED, Color.BLACK)
+        plot.addSeries(losses, lossesFormatter)
+        plot.setBackgroundColor(Color.WHITE)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
